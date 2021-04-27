2e59b7e34dbed51b9edfdd479586603313c9487c
RitaKr <ritokra@gmail.com>
2021-04-27 23:01:20 +0300

    редизайн главной страницы и нововведение - вакцина стран

--- main.js
+++ main.js
@@ -20,6 +20,217 @@ const arrow = document.querySelector('.arrow');
 const playerLevel = document.getElementById('level');
 const playerName = document.getElementById('name');
 
+
+let countries = [
+    "Абхазия",
+"Австралия",
+'Австрия' ,
+'Азербайджан' ,
+'Албания' ,
+'Алжир' ,
+'Ангола' ,
+'Андорра' ,
+'Антигуа и Барбуда' ,
+'Аргентина' ,
+'Армения' ,
+'Аруба' ,
+'Афганистан' ,
+'Багамы' ,
+'Бангладеш' ,
+'Барбадос' ,
+'Бахрейн' ,
+'Беларусь' ,
+'Белиз' ,
+'Бельгия' ,
+'Бенин' ,
+'Бермудские острова' ,
+'Болгария' ,
+'Боливия' ,
+'Босния и Герцеговина' ,
+'Ботсвана' ,
+'Бразилия' ,
+'Бруней-Даруссалам' ,
+'Буркина Фасо' ,
+'Бурунди' ,
+'Бутан' ,
+'Вануату' ,
+'Ватикан' ,
+'Великобритания',
+'Венгрия' ,
+'Венесуэла' ,
+'Вьетнам' ,
+'Габон' ,
+'Гавайские острова' ,
+'Гайана' ,
+'Гаити' ,
+'Гамбия' ,
+'Гана' ,
+'Гваделупа' ,
+'Гватемала' ,
+'Гвинея' ,
+'Гвинея-Биссау' ,
+'Германия' ,
+'Гондурас' ,
+'Гонконг',
+'Гренада' ,
+'Греция' ,
+'Грузия' ,
+'Дания' ,
+'Джибути' ,
+'Доминика'  ,
+'Доминикана' ,
+'Египет' ,
+'Замбия' ,
+'Зимбабве' ,
+'Израиль' ,
+'Индия' ,
+'Индонезия' ,
+'Иордания' ,
+'Ирак'  ,
+'Иран' ,
+'Ирландия' ,
+'Исландия' ,
+'Испания' ,
+'Италия' ,
+'Йемен' ,
+'Кабо-Верде' ,
+'Казахстан' ,
+'Каймановы острова' ,
+'Камбоджа ',
+'Камерун' ,
+'Канада' ,
+'Канарские острова'    ,
+'Катар' , 
+'Кения' ,
+'Кипр' ,
+'Киргизия' ,
+'Кирибати' ,
+'Китай' ,
+'Колумбия' ,
+'Коморы' ,
+'Конго' ,
+'Конго-Киншаса' ,
+'Коста-Рика' ,
+"Кот-д'Ивуар" ,
+'Куба' ,
+'Кувейт'  ,
+'Лаос' ,
+'Латвия' ,
+'Лесото',
+'Либерия' ,
+'Ливан' ,
+'Ливия' ,
+'Литва' ,
+'Лихтенштейн' ,
+'Люксембург' ,
+'Маврикий' ,
+'Мавритания' ,
+'Мадагаскар' ,
+'Малави' ,
+'Малайзия' ,
+'Мали' ,
+'Мальдивы ',
+'Мальта' ,
+'Марокко' ,
+'Мартиника' ,
+'Маршалловы острова' ,
+'Мексика' ,
+'Мозамбик' ,
+'Молдова' ,
+'Монако' ,
+'Монголия' ,
+'Мьянма' ,
+'Намибия' ,
+'Науру' ,
+'Непал' ,
+'Нигер' ,
+'Нигерия' ,
+'Нидерланды' ,
+'Никарагуа' ,
+'Новая Зеландия' ,
+'Норвегия' ,
+'ОАЭ' ,
+'Оман' ,
+'Остров Св. Елены' ,
+'Пакистан' ,
+'Палау' ,
+'Панама' ,
+'Папуа - Новая Гвинея' ,
+'Парагвай' ,
+'Перу' ,
+'Польша' ,
+'Португалия' ,
+'Пуэрто-Рико' ,
+'Реюньон' ,
+'Россия' ,
+'Руанда' ,
+'Румыния' ,
+'Сальвадор'  ,
+'Самоа' ,
+'Сан-Марино' ,
+'Сан-Томе и Принсипи' ,
+'Саудовская аравия' ,
+'Свазиленд ',
+'КНДР',
+'Северная Македония' ,
+'Сейшелы' ,
+'Сенегал' ,
+'Сен-Мартен ',
+'Сент-Винсент и Гренадины' ,
+'Сент-Китс и Невис' ,
+'Сент-Люсия' ,
+'Сербия' ,
+'Сингапур' ,
+'Сирия' ,
+'Словакия' ,
+'Словения' ,
+'Соломоновы острова' ,
+'Сомали' ,
+'Судан' ,
+'Суринам' ,
+'США' ,
+'Сьерра-Леоне' ,
+'Таджикистан' ,
+'Тайвань' ,
+'Тайланд' ,
+'Танзания' ,
+'Того' ,
+'Тонга' ,
+'Тринидад и Тобаго' ,
+'Тувалу' ,
+'Тунис' ,
+'Туркменистан' ,
+'Турция'  ,
+'Уганда' ,
+'Узбекистан' ,
+'Украина' ,
+'Уоллис и Футуна' ,
+'Уругвай' ,
+'Фиджи' ,
+'Филиппины' ,
+'Финляндия' ,
+'Франция' ,
+'Хорватия' ,
+'Центральноафриканская Республика' ,
+'Чад' ,
+'Черногория' ,
+'Чехия' ,
+'Чили' ,
+'Швейцария' ,
+'Швеция' ,
+'Шри-Ланка' ,
+'Эквадор' ,
+'Экваториальная Гвинея' ,
+'Эритрея' ,
+'Эстония' ,
+'Эфиопия' ,
+'ЮАР' ,
+'Южная Корея' ,
+"Ямайка" ,
+"Япония"
+]
+let completeCountries = []; 
+
 let isOpen = false;
 rulesBtn.addEventListener('click',()=>{
 
@@ -57,11 +268,20 @@ if (localStorage.playerLevel == null) {
 if (localStorage.playerName == null || localStorage.playerName == undefined) {
     setName('first');
 }
+if (localStorage.countries == null) {
+    localStorage.setItem('countries', '[]');    
+}
+if (localStorage.curCountry == null) {
+    localStorage.setItem('curCountry', decideRandomCounty(countries));  
+}
+if (localStorage.allCountries == null) {
+    localStorage.setItem('allCountries', JSON.stringify(countries));    
+}
 
 function setName(reason) {
     let name = "Игрок";
     if (reason=="first") {
-        name = prompt(`Добро пожаловать в CORONA GAMES! Вижу, Вы тут впервые? Введите ваш никнейм, перед тем как начать играть. По умолчанию установлено имя "Игрок", но вы можете поменять его в любое время на главной странице.`);
+        name = prompt(`Добро пожаловать в CORONA GAMES! Вижу, Вы тут впервые? Введите Ваш никнейм, перед тем как начать играть. По умолчанию установлено имя "Игрок", но Вы можете поменять его в любое время на главной странице.`);
         if (name==null || name==" "|| name=="") {
             name="Игрок"
         }
@@ -88,33 +308,74 @@ function updateAllLocalInners() {
     tpSpan.innerHTML= localStorage.tp;
     mskSpan.innerHTML= localStorage.msk;
     anSpan.innerHTML= localStorage.an;
-    vaccineValSpan.innerHTML= localStorage.vaccine;
-    vaccineLine.style.width = `${localStorage.vaccine}%`;
     playerLevel.innerHTML = localStorage.playerLevel+' ур.';
     playerName.innerHTML = localStorage.playerName;
+    vaccineValSpan.innerHTML= localStorage.vaccine;
+    vaccineLine.style.width = `${localStorage.vaccine}%`;
+    countrySpan.innerHTML = localStorage.curCountry;
     checkLevel()
     
 }
 
+function random(arr){
+    let n = Math.floor(Math.random()*(arr.length));
+    return arr[n]
+}
+function normalise(n) {
+    if (n<10) return '0'+n;
+    else return n
+}
+function calcDate(){
+    let date = new Date();
+    let dateString = `${normalise(date.getDate())}.${normalise(date.getMonth()+1)}.${normalise(date.getFullYear())} ${normalise(date.getHours())}:${date.getMinutes()}`
+    console.log(dateString)
+    return dateString
+}
+//let date = new Date();
+
+function decideRandomCounty(arr) {
+    let randomCountry = random(arr); 
+    arr.splice(arr.indexOf(randomCountry), 1);
+    return randomCountry
+}    
+function updateCountry(){
+    completeCountries = JSON.parse(localStorage.countries);
+    let aviableCountries = JSON.parse(localStorage.allCountries);
+    //completeCountries = JSON.parse(localStorage.countries);
+    //console.log(completeCountries, localStorage.countries);
+    let obj = {};
+    obj.country = localStorage.curCountry;
+    obj.date = calcDate();
+    completeCountries.push(obj);
+    //localStorage.countries = completeCountries
+    localStorage.countries = JSON.stringify(completeCountries)
+    localStorage.curCountry = decideRandomCounty(aviableCountries);
+    localStorage.allCountries =  JSON.stringify(aviableCountries)
+    
+    
+}
 function NullProgress() {
-    let answer = confirm('Вы уверенны что хотите аннулировать ваш прогресс? Будет аннулирован ваши рекорды во всех играх, все ваши запасы и вакцина.')
+    let answer = confirm('Вы уверенны что хотите аннулировать Ваш прогресс? Будет аннулирован ваши рекорды во всех играх, все ваши запасы и вакцина.')
     if (answer) {
-    localStorage.setItem('vaccine', 0);
-    localStorage.setItem('tp', 0);
-    localStorage.setItem('msk', 0);
-    localStorage.setItem('an', 0);
-    localStorage.setItem('CWbestScore', 0);
-    localStorage.setItem('CWtimesPlayed', 0);
-    localStorage.setItem('RPSwins', 0);
-    localStorage.setItem('RPSdraws', 0);
-    localStorage.setItem('RPSloses', 0);
-    localStorage.setItem('RPStimesPlayed', 0);
-    sessionStorage.setItem('RPSwinsStraight', 0);
-    localStorage.setItem('BObestScore', 0);
-    localStorage.setItem('BOtimesPlayed', 0);
-    localStorage.setItem('BObestLevel', 1);
-    localStorage.setItem('playerLevel', 1)
-    updateAllLocalInners()
+        localStorage.setItem('countries', '[]');
+        localStorage.setItem('curCountry', decideRandomCounty(countries));
+        localStorage.setItem('allCountries', JSON.stringify(countries));
+        localStorage.setItem('vaccine', 0);
+        localStorage.setItem('tp', 0);
+        localStorage.setItem('msk', 0);
+        localStorage.setItem('an', 0);
+        localStorage.setItem('CWbestScore', 0);
+        localStorage.setItem('CWtimesPlayed', 0);
+        localStorage.setItem('RPSwins', 0);
+        localStorage.setItem('RPSdraws', 0);
+        localStorage.setItem('RPSloses', 0);
+        localStorage.setItem('RPStimesPlayed', 0);
+        sessionStorage.setItem('RPSwinsStraight', 0);
+        localStorage.setItem('BObestScore', 0);
+        localStorage.setItem('BOtimesPlayed', 0);
+        localStorage.setItem('BObestLevel', 1);
+        localStorage.setItem('playerLevel', 1)
+        updateAllLocalInners()
     
     
     }
@@ -284,7 +545,8 @@ an.addEventListener('click',() =>{
     }
     function checkLevel(){
         if (localStorage.vaccine>=100) {
-                    localStorage.vaccine = 0;
-                    +localStorage.playerLevel++
+                localStorage.vaccine = 0;
+                +localStorage.playerLevel++;
+                updateCountry()
         }
     }
